30d5d9467ec1a22b2aa015ce4feb4faabe563ec4
Edward <e.arechwa@gmail.com>
2024-11-05 21:52:40 +0100

    feat: add config for env file

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,11 @@ import { FavoritesModule } from './modules/favorites/favorites.module';
 import { AlbumsModule } from './modules/albums/albums.module';
 import { TracksModule } from './modules/tracks/tracks.module';
 import { ArtistsModule } from './modules/artists/artists.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
     UsersModule,
     ArtistsModule,
     TracksModule,
