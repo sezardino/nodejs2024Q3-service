7024e291b9052f69126de5b90077d93ed7582a17
Edward <e.arechwa@gmail.com>
2024-11-05 21:40:17 +0100

    feat: add generated cli modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,19 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { FavoritesModule } from './favorites/favorites.module';
+import { AlbumsModule } from './albums/albums.module';
+import { TracksModule } from './tracks/tracks.module';
+import { ArtistsModule } from './artists/artists.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    UsersModule,
+    ArtistsModule,
+    TracksModule,
+    AlbumsModule,
+    FavoritesModule,
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
