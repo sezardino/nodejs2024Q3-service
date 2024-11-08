3e4ebe0b03051196249616d062d225b6d002c6f7
Edward <e.arechwa@gmail.com>
2024-11-08 13:54:28 +0100

    feat: add prisma to album

--- src/modules/albums/albums.module.ts
+++ src/modules/albums/albums.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
+import { PrismaService } from 'src/common/prisma';
 import { TracksModule } from '../tracks/tracks.module';
 import { AlbumsController } from './albums.controller';
 import { AlbumsService } from './albums.service';
 
 @Module({
   controllers: [AlbumsController],
-  providers: [AlbumsService],
+  providers: [AlbumsService, PrismaService],
   exports: [AlbumsService],
   imports: [TracksModule],
 })
